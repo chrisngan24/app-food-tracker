8e23baf215b17351541f1add9210cadbc59e001c
Chris Ngan <chris.ngan24@gmail.com>
2014-07-08 17:51:50 -0400

    refactored code

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
 import os
 from flask import Flask, request
 import db
-from entry import Entry
+from entries import Entries
 import json 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Bye World!'
+    return 'Hello World!'
 
 @app.route('/api/v1/add_entry', methods=['POST'])
 def add_food_entry():
     data_entry = json.loads(request.get_data())
     
-    entry = Entry()
-    entry.add_entry(data_entry) 
+    entries = Entries()
+    entries.add_entry(data_entry) 
     return 'success'
 
 
+"""
+@app.route('/api/v1/get_inventory', methods=['GET'])
+def get_inventory():
+      
+"""
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
+   
--- entries.py
+++ entries.py
@@ -1,25 +1,25 @@
-import entrydao
 import datetime
 import time
-import db
+import sys
+import  entriesdb
 
-class Entry:
+class Entries:
     def add_entry(self, data_entry): 
         date = datetime.date.fromtimestamp(eval(data_entry['time_in']))
         query = {'camera_id' : data_entry['camera_id'],
                  'date_in'   : str(date),
                  'item_name' : data_entry['item_name']}
-        same_entry = entrydao.find_same_entry(query)
+        same_entry = entriesdb.find_same_entry(query)
 
         document = query
 
         if same_entry == None:
             print document
             document['count'] = 1
-            entrydao.insert_entry(document)
+            entriesdb.insert_entry(document)
             
         else:
             same_entry['count'] += 1  
-            entrydao.update_entry({'_id' : same_entry['_id']},
+            entriesdb.update_entry({'_id' : same_entry['_id']},
                                      same_entry)
 
